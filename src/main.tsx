8b7f85f339d667fe8ae200929074f88cc2192507
RizalAlfadlil <rizalalfadlil@gmail.com>
2024-09-22 19:22:25 +0700

    change routes method

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Maker from "./Maker.tsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path:"/create",
-    element:<Maker/>
-  }
-]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/create" element={<Maker />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
